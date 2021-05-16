16d4fcff71ff5a647fb2a62df06919085d6a49f4
Camio38998 <luanmoonstar@gmail.com>
2021-05-16 22:16:03 +0000

    update repo

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 alert("Welcome to our app!");
 alert("We present to you......");
-alert("FreeSpace!!!")
+alert("FreeSpace!!!");
 
 document.addEventListener("click", main);
 function main(event) {
   var button = prompt("What is your Stress Level? From 1 - 3");
   if ((button === '1') || (buttton === '2') || (button === '3'));
+ 
 
 };
+// Level 1 https://gamejolt.net/?token=T8qv2KTQKV7sfhDYprEdB4KDecaGBF
+//level 2  reading
+//comdey https://www.wattpad.com/stories/comedy
+//fanastay https://www.wattpad.com/stories/fantasy
+// Level 3 https://youtu.be/W6YI3ZFOL0A
