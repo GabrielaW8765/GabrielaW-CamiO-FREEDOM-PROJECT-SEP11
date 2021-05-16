d2cf31ff64198bbbd0f7f03c9b504cbbf334bc56
Gabriela Wong <GabrielaW8765@hstat.org>
2021-05-16 22:15:48 +0000

    JS

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ alert("FreeSpace!!!")
 document.addEventListener("click", main);
 function main(event) {
   var button = prompt("What is your Stress Level? From 1 - 3");
-  if ((button === '1') || (buttton === '2') || (button === '3'));
+  if ((button === '1') || (button === '2') || (button === '3'));
 
 };
