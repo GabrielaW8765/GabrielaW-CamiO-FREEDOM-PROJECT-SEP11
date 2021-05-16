c4ec45966d6b82fc98de159fdf8b3e3230444c49
Camio38998 <luanmoonstar@gmail.com>
2021-05-16 22:47:44 +0000

    update repo

--- script.js
+++ script.js
@@ -5,13 +5,21 @@ alert("FreeSpace!!!");
 document.addEventListener("click", main);
 function main(event) {
   var button = prompt("What is your Stress Level? From 1 - 3");
-<<<<<<< HEAD
-  if ((button === '1') || (buttton === '2') || (button === '3'));
- 
-=======
-  if ((button === '1') || (button === '2') || (button === '3'));
->>>>>>> d2cf31ff64198bbbd0f7f03c9b504cbbf334bc56
-
+  if (button === '1') {
+    alert("It's is time to play Bubble pop!!!");
+    
+  } 
+  if (button === '2') {
+   var books = prompt("Would you like to read Comedy or Fanasty?");
+   if ((books === 'Comedy') || (books === 'comedy')){
+     
+   }else{
+     
+   };
+  }
+   if (button === '3') {
+    alert("Let's listen to some music ~ :) ")
+  }
 };
 // Level 1 https://gamejolt.net/?token=T8qv2KTQKV7sfhDYprEdB4KDecaGBF
 //level 2  reading
