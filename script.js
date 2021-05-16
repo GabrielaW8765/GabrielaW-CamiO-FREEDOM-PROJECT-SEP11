47e2e1f0e3bd1de6d53c9c7dfe2feda6864b4e2e
GabrielaWong <>
2021-05-16 21:28:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("Welcome to our app!");
+alert("We present to you......");
+alert("FreeSpace!!!")
+
+document.addEventListener("click", main);
+function main(event) {
+  var button = prompt("What is your Stress Level? From 1 - 3");
+  if ((button === '1') || (buttton === '2') || (button === '3'));
+
+};
